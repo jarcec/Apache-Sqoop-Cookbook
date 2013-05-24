8c3b5a6075e239b1a19066c1cadaafc518d462fd
Jarek Jarcec Cecho <jarcec@apache.org>
2013-05-24 06:28:34 -0700

    Improving MySQL script

--- mysql.tables.sql
+++ mysql.tables.sql
@@ -16,7 +16,7 @@
 
 -- Create table cities
 CREATE TABLE `cities` (
-  `id` INTEGER,
+  `id` INTEGER UNSIGNED NOT NULL,
   `country` VARCHAR(50),
   `city` VARCHAR(150),
   PRIMARY KEY (`id`)
@@ -27,9 +27,12 @@ INSERT INTO `cities`(`id`, `country`, `city`) VALUES (1, "USA", "Palo Alto");
 INSERT INTO `cities`(`id`, `country`, `city`) VALUES (2, "Czech Republic", "Brno");
 INSERT INTO `cities`(`id`, `country`, `city`) VALUES (3, "USA", "Sunnyvale");
 
+-- Create staging table for export
+CREATE TABLE `staging_cities` LIKE `cities`;
+
 -- Create table countries
 CREATE TABLE `countries` (
-  `country_id` INTEGER,
+  `country_id` INTEGER UNSIGNED NOT NULL,
   `country` VARCHAR(50),
   PRIMARY KEY (`country_id`)
 );
@@ -40,8 +43,8 @@ INSERT INTO `countries`(`country_id`, `country`) VALUES (2, "Czech Republic");
 
 -- Create normalized variant of table cities
 CREATE TABLE `normcities` (
-  `id` INT,
-  `country_id` INT,
+  `id` INTEGER UNSIGNED NOT NULL,
+  `country_id` INTEGER UNSIGNED NOT NULL,
   `city` VARCHAR(150),
   PRIMARY KEY (`id`),
   FOREIGN KEY (`country_id`) REFERENCES `countries`(`country_id`)
@@ -51,3 +54,16 @@ CREATE TABLE `normcities` (
 INSERT INTO `normcities`(`id`, `country_id`, `city`) VALUES (1, 1, "Palo Alto");
 INSERT INTO `normcities`(`id`, `country_id`, `city`) VALUES (2, 2, "Brno");
 INSERT INTO `normcities`(`id`, `country_id`, `city`) VALUES (3, 1, "Sunnyvale");
+
+-- Create visits table that is suitable for incremental import
+CREATE TABLE `visits` (
+  `id` INTEGER UNSIGNED NOT NULL,
+  `city` VARCHAR(50),
+  `last_update_date` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY (`last_update_date`)
+);
+
+-- Insert couple of records to table visits
+INSERT INTO `visits`(`id`, `city`, `last_update_date`) VALUES(1, "Freemont", "1983-05-22 01:01:01");
+INSERT INTO `visits`(`id`, `city`, `last_update_date`) VALUES(2, "Jicin", "1987-02-02 02:02:02");
