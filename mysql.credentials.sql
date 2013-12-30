e9e2432b48a6714581f0f7a44cb8f5ea0e201e48
Sho Shimauchi <sho.shimauchi@gmail.com>
2013-12-30 16:38:58 +0900

    add a new mysql user for localhost

--- mysql.credentials.sql
+++ mysql.credentials.sql
@@ -22,3 +22,9 @@ CREATE DATABASE sqoop;
 
 -- Grant all privileges to the new database to created user
 GRANT ALL ON sqoop.* to 'sqoop'@'%';
+
+-- in case MySQL runs on local machine
+CREATE USER 'sqoop'@'localhost' IDENTIFIED BY 'sqoop';
+GRANT ALL ON sqoop.* TO 'sqoop'@'localhost';
+
+
